deb8296a7ba4fb5b2e50d3a6cf16a4398ba068b9
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-07-01 21:46:00 +0200

    Add a couple of plain TextInputs

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.header}>Regular text inputs</Text>
+      <TextInput placeholder="Name" style={styles.input} />
+      <TextInput placeholder="Address" style={styles.input} />
     </View>
   );
 }
@@ -14,8 +14,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#bebec3',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    padding: 12,
+    backgroundColor: '#fff',
+    marginBottom: 8,
+  },
+  header: {
+    fontSize: 18,
+    marginBottom: 8,
+  }
 });
