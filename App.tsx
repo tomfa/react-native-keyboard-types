5ee449a4a00c701284c5d94b744f6cd03654cb83
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-07-02 09:23:17 +0200

    Render all keyboard types

--- App.tsx
+++ App.tsx
@@ -1,36 +1,55 @@
-import React from "react";
-import { StyleSheet, Text, View, TextInput } from "react-native";
+import React, { useMemo } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.header}>Regular text inputs</Text>
-      <TextInput
-        placeholder="Name"
-        style={styles.input}
-      />
-      <TextInput
-        placeholder="Address"
-        style={styles.input}
-      />
-    </View>
-  );
+import KeyBoard, { KeyboardType } from './Keyboard';
+import { useInputFocus } from "./hooks";
+
+interface KeyBoardPropType {
+  type: KeyboardType;
+  nextType?: KeyboardType;
+  autoFocus: boolean;
 }
 
+const App = () => {
+  const { focus, setRef } = useInputFocus();
+
+  const keyBoardProps: Array<KeyBoardPropType> = useMemo(() => {
+    const allKeyboardTypes: Array<KeyboardType> = Object.values(KeyboardType);
+    return allKeyboardTypes
+      .reverse()
+      .reduce((acc: Array<KeyBoardPropType>, type) => {
+        const nextType = acc.length > 0 ? acc[acc.length - 1].type : undefined;
+        const props: KeyBoardPropType = {
+          type,
+          nextType,
+          autoFocus: acc.length === Object.values(KeyboardType).length - 1,
+        };
+        acc.push(props);
+        return acc;
+      }, [])
+      .reverse();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>React Native keyboardType demo</Text>
+      {keyBoardProps.map(({ type, nextType, autoFocus }) => (
+        <KeyBoard
+          key={type}
+          type={type}
+          myRef={setRef(type)}
+          onSubmitEditing={() => !!nextType && focus(nextType)}
+          autoFocus={autoFocus}
+        />
+      ))}
+    </View>
+  );
+};
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#bebec3",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  input: {
-    padding: 12,
-    backgroundColor: "#fff",
-    marginBottom: 8,
-  },
-  header: {
-    fontSize: 18,
-    marginBottom: 8,
-  },
+  container: { backgroundColor: 'hotpink', padding: 10, minHeight: '100%' },
+  header: { fontSize: 32, marginBottom: 20 }
 });
+
+
+export default App;
